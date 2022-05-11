5e1428284468efe286c5c8e2c930cb5ea0d62baa
Marcelo <marcelolopezhernandez@gmail.com>
2022-05-11 00:17:31 -0400

    Fix Routes

---
+++ src/pages/Progress/Progress.js
@@ -0,0 +1,35 @@
+import { ConfigContext } from "../../providers/ConfigProvider";
+
+import React, { useContext } from "react";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import "../../scss/style.scss";
+import { Typography } from "@mui/material";
+import Fade from "react-reveal/Fade";
+
+export default function Progress() {
+  const idioma = useContext(ConfigContext);
+
+  console.log(idioma)
+  return (
+    <Container className="bgSoluttoCirco" style={{height:"60vh",paddingTop:"1rem"}}>
+      <Grid
+        container
+        spacing={12}
+        style={{
+          justifyContent: "right",
+          marginBottom: "3rem",
+          marginTop: "1px",
+        }}
+      >
+        <Grid item xs={12} md={12}>
+          <Fade top>
+            <Typography variant="h1" class="pageTitle" style={{textAlign:"center"}}>
+              {idioma.lang === "esp" ? "Sitio en Construcción" : "On Working"}
+            </Typography>
+          </Fade>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
